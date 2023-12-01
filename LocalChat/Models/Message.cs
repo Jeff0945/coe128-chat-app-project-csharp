0394516d37d95da0db61bdc930ff2d6b6a77b5de
Jeff Froilan Fortunado <jeff0945@gmail.com>
2023-12-02 01:04:44 +0800

    Add Update function and Validation

--- LocalChat/Models/Message.cs
+++ LocalChat/Models/Message.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq;
+using System.Windows.Forms;
 using LocalChat.Services;
 using LocalChat.Interfaces;
 
@@ -6,12 +7,14 @@ namespace LocalChat.Models
 {
     public class Message : ICollection
     {
-        public int Id { get; set; }
-        public string UserName { get; set; }
-        public string Text { get; set; }
+        public int Id { get; }
+        public string UserName { get; }
+        public string Text { get; private set; }
 
         public Message(string userName, string message)
         {
+            if (!Validate(userName)) return;
+            
             Id = GenerateId();
             UserName = userName;
             Text = message;
@@ -26,14 +29,29 @@ namespace LocalChat.Models
                 .First(user => user.UserName == UserName);
         }
 
+        public Message Update(string message)
+        {
+            Text = message;
+            
+            return this;
+        }
+
+        private bool Validate(string userName)
+        {
+            bool exists = Data.Database
+                .Users()
+                .Any(user => user.UserName == userName);
+
+            if (!exists) MessageBox.Show("User does not exist.");
+
+            return exists;
+        }
+
         private int GenerateId()
         {
             var messages = Data.Database.Messages().ToList();
 
-            if (!messages.Any())
-            {
-                return 1;
-            }
+            if (!messages.Any()) return 1;
 
             return messages.Last().Id + 1;
         }
--- LocalChat/Models/User.cs
+++ LocalChat/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Windows.Forms;
 using LocalChat.Components.UsersList;
 using LocalChat.Interfaces;
 using LocalChat.Services;
@@ -8,11 +9,13 @@ namespace LocalChat.Models
 {
     public class User : ICollection
     {
-        public string UserName { get; set; }
-        public string Name { get; set; }
+        public string UserName { get; private set; }
+        public string Name { get; private set; }
 
         public User(string userName, string name, Chats instance)
         {
+            if (!Validate(userName)) return;
+            
             UserName = userName;
             Name = name;
             
@@ -25,6 +28,27 @@ namespace LocalChat.Models
             return Data.Database.Messages()
                 .Where(message => message.UserName == UserName)
                 .ToList();
+        }
+
+        public User Update(string userName = null, string name = null)
+        {
+            if (!Validate(userName)) return this;
+
+            if (userName != null) UserName = userName;
+            if (name != null) Name = name;
+
+            return this;
+        }
+
+        private bool Validate(string userName)
+        {
+            var exists = Data.Database
+                .Users()
+                .Any(user => user.UserName == userName && user != this);
+
+            if (exists) MessageBox.Show($"{userName} already exists.");
+
+            return !exists;
         }
     }
 }
